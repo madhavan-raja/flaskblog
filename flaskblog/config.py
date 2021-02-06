cd6749654021241018a2785e59deb0ee9036cf5a
Madhavan Raja <madhavanraja99@gmail.com>
2021-02-06 23:10:38 +0530

    Exported everything to .env, added 405 error page

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,12 +1,19 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY = '8754095ed89cdbeb83761fb010f59f42'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or ''
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or ''
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    TESTING = os.getenv('FLASK_DEBUG')
+    DEBUG = os.getenv('DEBUG')
+    SERVER = os.getenv('SERVER')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_DEBUG = os.getenv('MAIL_DEBUG')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
